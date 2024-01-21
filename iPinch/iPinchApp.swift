77c93b89ac6d62377fc632932ed3edf7e0a9d331
Elfana, A | Elfaael <elfaael12@gmail.com>
2024-01-21 13:00:15 +0700

    Initial Commit

---
+++ iPinch/iPinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPinchApp.swift
+//  iPinch
+//
+//  Created by Elfana Anamta Chatya on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iPinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
